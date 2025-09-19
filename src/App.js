1e52faf91ccb2d101198a55f76f1ff3ef5f31788
Tamilarasan135 <ktamilarasan333@gmail.com>
2025-09-19 17:36:24 +0530

    Initial commit: React + Tailwind + CI workflow

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
+    
+     <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold mb-6 text-center text-indigo-600">
+        🚀 CI/CD Dashboard Demo
+      </h1>
+
+     <div className="min-h-screen bg-gray-100 p-6">
+      
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {/* Box 1 */}
+        <div className="bg-white shadow-lg rounded-2xl p-6">
+          <h2 className="text-xl font-semibold mb-2">Family nation</h2>
+          <p className="text-green-600 font-bold">Family nation</p>
+        </div>
+
+        {/* Box 2 */}
+        <div className="bg-white shadow-lg rounded-2xl p-6">
+          <h2 className="text-xl font-semibold mb-2">Deployments</h2>
+          <p className="text-blue-600 font-bold">Technical Assitance</p>
+        </div>
+
+        {/* Box 3 */}
+        <div className="bg-white shadow-lg rounded-2xl p-6">
+          <h2 className="text-xl font-semibold mb-2">Health Check</h2>
+          <p className="text-green-600 font-bold">Healthy</p>
+        </div>
+
+        {/* Box 4 */}
+        <div className="bg-white shadow-lg rounded-2xl p-6">
+          <h2 className="text-xl font-semibold mb-2">Slack Alerts</h2>
+          <p className="text-yellow-600 font-bold">2 Pending</p>
+        </div>
+
+        {/* Box 5 */}
+        <div className="bg-white shadow-lg rounded-2xl p-6">
+          <h2 className="text-xl font-semibold mb-2">conference room </h2>
+          <button className="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded-lg">
+            Rollback Last Release
+          </button>
+        </div>
+
+        {/* Box 6 */}
+        <div className="bg-white shadow-lg rounded-2xl p-6">
+          <h2 className="text-xl font-semibold mb-2">Logs</h2>
+          <p className="text-gray-600">Family</p>
+        </div>
+      </div>
+    </div>
+    </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +67,12 @@ function App() {
           Learn React
         </a>
       </header>
+     
+      <h1 className="text-1xl text-red-700  underline underline">
+      Hello world!
+    </h1>
+
+    
     </div>
   );
 }
